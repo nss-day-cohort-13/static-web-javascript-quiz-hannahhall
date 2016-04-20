8edd80484f0a6056daf0e02ed4ed221cd1986f63
Hannah Hall <hannahkhall13@gmail.com>
2016-04-20 10:03:19 -0500

    finally made a tree shape

--- quiz.js
+++ quiz.js
@@ -6,28 +6,34 @@ var button = document.getElementById("button");
 function tree(branches, leaf) {
 	if (branchNum.value==="" || leafNum.value===""){
 		alert("Both fields must have a value");
-	} else {
+		} else{
+		for(j=0; j<branches; j++) {
+			grow.unshift(" ");
+		}
 		for(i=0; i<branches; i++) {
 			grow.push(leaf);
-			grow.unshift(" ");
-			console.log(grow);
+			grow.push(leaf);
+			grow.shift(" ");
+			console.log(grow.join(""));
 		}
+
 	}
 }
 function growTree (clickEvent) {
 	tree(branchNum.value, leafNum.value);
 }
-
-button.addEventListener("click", growTree);
-leafNum.addEventListener("keydown", enterKey);
-branchNum.addEventListener("keydown", enterKey);
-
 function enterKey(e) {
   var keyCode = e.keyCode;
   if(keyCode==13) {
   		tree(branchNum.value, leafNum.value);
   }
 }
+
+button.addEventListener("click", growTree);
+leafNum.addEventListener("keydown", enterKey);
+branchNum.addEventListener("keydown", enterKey);
+
+
 
 
 
