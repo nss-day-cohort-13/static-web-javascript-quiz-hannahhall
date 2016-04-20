33079d0cb1f1934ec1f5f87b6e6355cd3f9fcf7b
Hannah Hall <hannahkhall13@gmail.com>
2016-04-19 21:47:59 -0500

    added event listeners

--- quiz.js
+++ quiz.js
@@ -1,13 +1,27 @@
 var grow= [];
-
 function tree(branches, leaf) {
 	for(i=0; i<branches; i++) {
-		 grow.push(leaf);
-		 grow.unshift(" ");
-
-		 console.log(grow.join(""));
+		grow.push(leaf);
+		grow.unshift(" ");
+		console.log(grow.join(""));
 	}
-
+}
+var button = document.getElementById("button");
+function growTree (clickEvent) {
+	tree(document.getElementById("branches").value, document.getElementById("leaf").value);
+}
+button.addEventListener("click", growTree);
+document.getElementById("leaf").addEventListener("keydown", enterKey, false);
+function enterKey(e) {
+  var keyCode = e.keyCode;
+  if(keyCode==13) {
+  		tree(document.getElementById("branches").value, document.getElementById("leaf").value);
+  }
 }
 
-tree(5, "*");
+
+
+
+
+
+
