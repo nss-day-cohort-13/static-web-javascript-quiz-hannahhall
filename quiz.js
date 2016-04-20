abccf5fbf1d24444cbb4f5e8470445cf16c7be44
Hannah Hall <hannahkhall13@gmail.com>
2016-04-20 13:10:17 -0500

    fixed tree function single object

--- quiz.js
+++ quiz.js
@@ -2,19 +2,22 @@ var grow= [];
 var branchNum= document.getElementById("branches");
 var leafNum= document.getElementById("leaf");
 var button = document.getElementById("button");
-
-function tree(branches, leaf) {
-	if (branchNum.value==="" || leafNum.value===""){
+var input = {
+	branches: "",
+	leaf: ""
+};
+function tree(input) {
+	if (input.branches==="" || input.leaf===""){
 		alert("Both fields must have a value");
 		} else{
-		for(j=0; j<branches-1; j++) {
+		for(j=0; j<input.branches-1; j++) {
 			grow.unshift(" ");
 		}
-		grow.push(leaf);
+		grow.push(input.leaf);
 		console.log(grow.join(""));
-		for(i=0; i<branches-1; i++) {
-			grow.push(leaf);
-			grow.push(leaf);
+		for(i=0; i<input.branches-1; i++) {
+			grow.push(input.leaf);
+			grow.push(input.leaf);
 			grow.shift(" ");
 			console.log(grow.join(""));
 		}
@@ -22,25 +25,19 @@ function tree(branches, leaf) {
 	}
 }
 function growTree (clickEvent) {
-	tree(branchNum.value, leafNum.value);
+	input.branches= branchNum.value
+	input.leaf = leafNum.value
+	tree(input);
 }
 function enterKey(e) {
+	input.branches= branchNum.value
+	input.leaf = leafNum.value
 	var keyCode = e.keyCode;
 	if(keyCode==13) {
-		tree(branchNum.value, leafNum.value);
+		tree(input);
 	}
 }
 
 button.addEventListener("click", growTree);
 leafNum.addEventListener("keydown", enterKey);
 branchNum.addEventListener("keydown", enterKey);
-
-
-
-
-
-
-
-
-
-
